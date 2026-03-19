e7a7c70582e739a51f2312372058320f9f5a3a4a
pepms1 <125699227+pepms1@users.noreply.github.com>
2026-03-19 11:45:01 -0600

    Pinta en azul y aumenta 50% el total sin IVA filtrado

--- web/src/ui/SearchTransactionsV2.jsx
+++ web/src/ui/SearchTransactionsV2.jsx
@@ -385,7 +385,7 @@ export function SearchTransactionsV2({ cats, vendors, selectedProjectId }) {
           {exporting ? 'Exportando...' : 'Exportar PDF'}
         </button>
       </div>
-      <div className="small" style={{ marginTop: 6 }}>
+      <div className="small" style={{ marginTop: 6, fontSize: '1.5em', color: 'blue' }}>
         Total sin IVA filtrado: {formatCurrency(totalWithoutTax)}
       </div>
 
